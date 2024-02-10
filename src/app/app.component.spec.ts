7566f221fbe7a71abe621dddf7be271118ae4b11
Jaime Pineda <jpin730@pm.me>
2024-02-10 00:42:40 -0600

    refactor: :fire: remove scaffolding

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing'
+
 import { AppComponent } from './app.component'
 
 describe('AppComponent', () => {
@@ -12,20 +13,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent)
     const app = fixture.componentInstance
     expect(app).toBeTruthy()
-  })
-
-  it(`should have the 'angular-poke-trainer' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent)
-    const app = fixture.componentInstance
-    expect(app.title).toEqual('angular-poke-trainer')
-  })
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent)
-    fixture.detectChanges()
-    const compiled = fixture.nativeElement as HTMLElement
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, angular-poke-trainer',
-    )
   })
 })
