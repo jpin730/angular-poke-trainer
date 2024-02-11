57b7d01f1ac2dc6d8288cf637059f94b1b373a5c
Jaime Pineda <jpin730@pm.me>
2024-02-10 22:16:23 -0600

    refactor: :fire: remove unused imports

--- src/app/home/components/home-page/home-page.component.ts
+++ src/app/home/components/home-page/home-page.component.ts
@@ -3,7 +3,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core'
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/pokemons/components/pokemons-page/pokemons-page.component.ts
+++ src/app/pokemons/components/pokemons-page/pokemons-page.component.ts
@@ -3,7 +3,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core'
 @Component({
   selector: 'app-pokemons-page',
   standalone: true,
-  imports: [],
   templateUrl: './pokemons-page.component.html',
   styleUrl: './pokemons-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
