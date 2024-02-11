938e783a44e33842ed0a7bf81f0f99bcd801b67c
Jaime Pineda <jpin730@pm.me>
2024-02-10 19:48:16 -0600

    refactor: :zap: change detection strategy in some components

--- src/app/shared/components/main-layout/main-layout.component.ts
+++ src/app/shared/components/main-layout/main-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 
 import { HeaderComponent } from '../header/header.component'
@@ -9,5 +9,6 @@ import { HeaderComponent } from '../header/header.component'
   imports: [RouterOutlet, HeaderComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainLayoutComponent {}
