842b81c5539608314ccb275426710ac14be42108
Jaime Pineda <jpin730@pm.me>
2024-02-10 22:15:18 -0600

    feat: :construction: add profile form

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,45 @@
 import { NgModule } from '@angular/core'
 
+import { LuxonDateModule } from '@angular/material-luxon-adapter'
+import { MatAutocompleteModule } from '@angular/material/autocomplete'
 import { MatButtonModule } from '@angular/material/button'
+import { MatChipsModule } from '@angular/material/chips'
+import {
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+  MatDateFormats,
+} from '@angular/material/core'
+import { MatDatepickerModule } from '@angular/material/datepicker'
+import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatIconModule } from '@angular/material/icon'
+import { MatInputModule } from '@angular/material/input'
+
+const APP_DATE_FORMATS: MatDateFormats = {
+  parse: {
+    dateInput: 'yyyy-MM-dd',
+  },
+  display: {
+    dateInput: 'dd/MMM/yyyy',
+    monthYearLabel: 'MMMM yyyy',
+    dateA11yLabel: 'MMM',
+    monthYearA11yLabel: 'MMMM yyyy',
+  },
+}
 
 @NgModule({
-  exports: [MatButtonModule, MatIconModule],
+  exports: [
+    LuxonDateModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+  ],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'es' },
+    { provide: MAT_DATE_FORMATS, useValue: APP_DATE_FORMATS },
+  ],
 })
 export class MaterialModule {}
--- src/app/profile/components/profile-form/profile-form.component.ts
+++ src/app/profile/components/profile-form/profile-form.component.ts
@@ -1,10 +1,103 @@
-import { Component } from '@angular/core'
+import { ENTER } from '@angular/cdk/keycodes'
+import { CommonModule } from '@angular/common'
+import { Component, OnInit, inject } from '@angular/core'
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete'
+import { MatChipInputEvent } from '@angular/material/chips'
+import { DateTime } from 'luxon'
+import { Observable, distinctUntilChanged, map, startWith, tap } from 'rxjs'
+
+import { MaterialModule } from '@app-material/material.module'
+import { getAge } from '@core/helpers/get-age.helper'
 
 @Component({
   selector: 'app-profile-form',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, ReactiveFormsModule, CommonModule],
   templateUrl: './profile-form.component.html',
   styleUrl: './profile-form.component.scss',
 })
-export class ProfileFormComponent {}
+export class ProfileFormComponent implements OnInit {
+  private readonly fb = inject(FormBuilder).nonNullable
+
+  profileForm = this.fb.group({
+    name: ['', [Validators.required]],
+    hobby: this.fb.control<string[]>([], [Validators.required]),
+    birthday: this.fb.control<DateTime | ''>('', [Validators.required]),
+    document: ['', []],
+  })
+  hobbyInputControl = this.fb.control<string>('')
+  separatorKeysCodes: number[] = [ENTER]
+  today = DateTime.now()
+  filteredHobbies$!: Observable<string[]>
+  isAdult$!: Observable<boolean>
+  hobbyList: string[] = [
+    'Play Soccer',
+    'Play Basketball',
+    'Play Tennis',
+    'Play Volleyball',
+    'Play Video Games',
+  ]
+
+  get hobbyControl() {
+    return this.profileForm.controls.hobby
+  }
+
+  get birthdayControl() {
+    return this.profileForm.controls.birthday
+  }
+
+  get documentControl() {
+    return this.profileForm.controls.document
+  }
+
+  ngOnInit(): void {
+    this.filteredHobbies$ = this.hobbyInputControl.valueChanges.pipe(
+      startWith(null),
+      map((hobby: string | null) =>
+        hobby ? this._filter(hobby) : this.hobbyList.slice(),
+      ),
+    )
+
+    this.isAdult$ = this.birthdayControl.valueChanges.pipe(
+      map((birthday) => birthday !== '' && getAge(birthday) >= 18),
+      distinctUntilChanged(),
+      tap((isAdult) =>
+        isAdult
+          ? this.documentControl.addValidators(Validators.required)
+          : this.documentControl.removeValidators(Validators.required),
+      ),
+    )
+  }
+
+  submitProfile() {
+    if (this.profileForm.invalid) return
+
+    this.profileForm.getRawValue()
+  }
+
+  addHobby(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim()
+
+    if (value) this.hobbyControl.setValue([value])
+
+    event.chipInput.clear()
+  }
+
+  removeHobby() {
+    this.hobbyControl.setValue([])
+    this.hobbyInputControl.setValue('')
+  }
+
+  selectedHobby(event: MatAutocompleteSelectedEvent): void {
+    this.hobbyControl.setValue([event.option.viewValue])
+    this.hobbyInputControl.setValue('')
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase()
+    return this.hobbyList.filter((hobby) =>
+      hobby.toLowerCase().includes(filterValue),
+    )
+  }
+}
