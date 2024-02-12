0b6af5decdc9a2cf4748aceb5b4df0dff5e6472e
Jaime Pineda <jpin730@pm.me>
2024-02-12 16:54:15 -0600

    feat: :lipstick: add layout to home page

--- src/app/home/components/home-page/home-page.component.spec.ts
+++ src/app/home/components/home-page/home-page.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
+import { PokeTrainerService } from '@core/services/poke-trainer.service'
 import { HomePageComponent } from './home-page.component'
+
+class MockedPokeTrainerService {}
 
 describe('HomePageComponent', () => {
   let component: HomePageComponent
@@ -9,6 +12,9 @@ describe('HomePageComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HomePageComponent],
+      providers: [
+        { provide: PokeTrainerService, useClass: MockedPokeTrainerService },
+      ],
     }).compileComponents()
 
     fixture = TestBed.createComponent(HomePageComponent)
--- src/app/home/components/home-page/home-page.component.ts
+++ src/app/home/components/home-page/home-page.component.ts
@@ -1,10 +1,38 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core'
+import { RouterLink } from '@angular/router'
+
+import { MaterialModule } from '@app-material/material.module'
+import { PATH } from '@core/constants/path.constant'
+import { Pokemon } from '@core/interfaces/pokemon.interface'
+import { Trainer } from '@core/interfaces/trainer.interface'
+import { PokeTrainerService } from '@core/services/poke-trainer.service'
+import { ProfileComponent } from '@shared/components/profile/profile.component'
+import { TrainerCardComponent } from '@shared/components/trainer-card/trainer-card.component'
+import { FirstWordPipe } from '@shared/pipes/first-word.pipe'
+import { PokemonOverviewComponent } from '../pokemon-overview/pokemon-overview.component'
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
+  imports: [
+    CommonModule,
+    FirstWordPipe,
+    MaterialModule,
+    PokemonOverviewComponent,
+    ProfileComponent,
+    RouterLink,
+    TrainerCardComponent,
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  private readonly pokeTrainerService = inject(PokeTrainerService)
+
+  PATH = PATH
+
+  pokemons = this.pokeTrainerService.pokemons as Pokemon[]
+  trainer = this.pokeTrainerService.trainer as Trainer
+}
