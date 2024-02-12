86374669106ea1bfedd6abdc9e8abd8d2d3416f8
Jaime Pineda <jpin730@pm.me>
2024-02-12 11:54:50 -0600

    test: :white_check_mark: pass tests

--- src/app/pokemons/components/pokemon-checkbox/pokemon-checkbox.component.spec.ts
+++ src/app/pokemons/components/pokemon-checkbox/pokemon-checkbox.component.spec.ts
@@ -1,6 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
+import { Pokemon } from '@core/interfaces/pokemon.interface'
 import { PokemonCheckboxComponent } from './pokemon-checkbox.component'
+
+const mockedPokemon: Pokemon = {
+  id: 1,
+  name: 'bulbasaur',
+  sprite: 'https://example.com/bulbasaur.png',
+  type: 'grass',
+  hp: 45,
+  attack: 49,
+  defense: 49,
+  specialAttack: 65,
+  specialDefense: 65,
+  speed: 45,
+}
 
 describe('PokemonCheckboxComponent', () => {
   let component: PokemonCheckboxComponent
@@ -13,6 +27,7 @@ describe('PokemonCheckboxComponent', () => {
 
     fixture = TestBed.createComponent(PokemonCheckboxComponent)
     component = fixture.componentInstance
+    component.pokemon = mockedPokemon
     fixture.detectChanges()
   })
 
