b7bbb1b0a45fe0b18c6647d099c0c2e8fb6711e5
Jaime Pineda <jpin730@pm.me>
2024-02-12 11:48:34 -0600

    feat: :sparkles: add main behaviour in pokemon selector

---
+++ src/app/core/constants/max-pokemon-selection.constant.ts
@@ -0,0 +1,1 @@
+export const MAX_POKEMON_SELECTION = 3
--- src/app/pokemons/components/pokemon-selector/pokemon-selector.component.ts
+++ src/app/pokemons/components/pokemon-selector/pokemon-selector.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, inject } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 import { Observable, map } from 'rxjs'
 
+import { MAX_POKEMON_SELECTION } from '@core/constants/max-pokemon-selection.constant'
 import { Pokemon } from '@core/interfaces/pokemon.interface'
 import { PokemonCheckboxComponent } from '../pokemon-checkbox/pokemon-checkbox.component'
 
@@ -17,8 +18,24 @@ export class PokemonSelectorComponent implements OnInit {
   private readonly route = inject(ActivatedRoute)
 
   pokemons$!: Observable<Pokemon[]>
+  selectedPokemons: number[] = []
+  disabledCheckboxes = false
 
   ngOnInit() {
     this.pokemons$ = this.route.data.pipe(map((data) => data['pokemons'] ?? []))
+  }
+
+  onChangePokemonSelection(event: Event) {
+    const { checked, value } = event.target as HTMLInputElement
+
+    if (checked) {
+      this.selectedPokemons.push(+value)
+    } else {
+      const index = this.selectedPokemons.findIndex((id) => id === +value)
+      this.selectedPokemons.splice(index, 1)
+    }
+
+    this.disabledCheckboxes =
+      this.selectedPokemons.length === MAX_POKEMON_SELECTION
   }
 }
