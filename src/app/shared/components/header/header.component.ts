92cac7e61693ba0a23efc0bded93aff065974181
Jaime Pineda <jpin730@pm.me>
2024-02-13 01:36:18 -0600

    feat: :lipstick: add buttons in header component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,28 @@
-import { NgOptimizedImage } from '@angular/common'
-import { Component } from '@angular/core'
+import { CommonModule, NgOptimizedImage } from '@angular/common'
+import { Component, inject } from '@angular/core'
+import { MaterialModule } from '@app-material/material.module'
+
+import { PATH } from '@core/constants/path.constant'
+import { Trainer } from '@core/interfaces/trainer.interface'
+import { PokeTrainerService } from '@core/services/poke-trainer.service'
+import { FirstWordPipe } from '@shared/pipes/first-word.pipe'
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, CommonModule, MaterialModule, FirstWordPipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private readonly pokeTrainerService = inject(PokeTrainerService)
+
+  PATH = PATH
+
+  trainer: Trainer | null = null
+
+  get currentUrl() {
+    this.trainer = this.pokeTrainerService.trainer
+    return location.pathname
+  }
+}
