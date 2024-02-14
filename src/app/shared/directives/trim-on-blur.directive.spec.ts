a845709bcac045b70173626b5a727f09ecda5e16
Jaime Pineda <jpin730@pm.me>
2024-02-13 23:05:18 -0600

    test: :white_check_mark: add test to trim on blur component

--- src/app/shared/directives/trim-on-blur.directive.spec.ts
+++ src/app/shared/directives/trim-on-blur.directive.spec.ts
@@ -1,25 +1,33 @@
-import { Component } from '@angular/core'
+import { Component, DebugElement } from '@angular/core'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { By } from '@angular/platform-browser'
 
+import { FormControl, ReactiveFormsModule } from '@angular/forms'
 import { TrimOnBlurDirective } from './trim-on-blur.directive'
 
 @Component({
-  template: `<input appTrimOnBlur />`,
+  template: `<input appTrimOnBlur [formControl]="formControl" />`,
 })
-class HostComponent {}
+class HostComponent {
+  formControl = new FormControl()
+}
 
 describe('TrimOnBlurDirective', () => {
   let component: HostComponent
   let fixture: ComponentFixture<HostComponent>
+  let input: DebugElement
+  let directive: TrimOnBlurDirective
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HostComponent],
-      imports: [TrimOnBlurDirective],
+      imports: [TrimOnBlurDirective, ReactiveFormsModule],
     }).compileComponents()
 
     fixture = TestBed.createComponent(HostComponent)
     component = fixture.componentInstance
+    input = fixture.debugElement.query(By.css('input'))
+    directive = input.injector.get(TrimOnBlurDirective)
     fixture.detectChanges()
   })
 
@@ -28,10 +36,25 @@ describe('TrimOnBlurDirective', () => {
   })
 
   it('should have directive', () => {
-    const directive = fixture.debugElement
-      .query((el) => el.name === 'input')
-      .injector.get(TrimOnBlurDirective)
-
     expect(directive).toBeTruthy()
+  })
+
+  it('should not trim value on blur without control', () => {
+    const text = '  test  '
+
+    input.nativeElement.value = text
+    input.triggerEventHandler('blur')
+
+    expect(input.nativeElement.value).toBe(text)
+  })
+
+  it('should trim value on blur when control exists', () => {
+    const text = '  test  '
+
+    component.formControl.setValue(text)
+    input.nativeElement.value = text
+    input.triggerEventHandler('blur')
+
+    expect(input.nativeElement.value).toBe(text.trim())
   })
 })
