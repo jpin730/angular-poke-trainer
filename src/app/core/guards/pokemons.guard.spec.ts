8373aaadb9ef80ebb3a0eb76c106ae284f891b5c
Jaime Pineda <jpin730@pm.me>
2024-02-13 20:17:20 -0600

    test: :white_check_mark: add tests to pokemons guard

--- src/app/core/guards/pokemons.guard.spec.ts
+++ src/app/core/guards/pokemons.guard.spec.ts
@@ -1,17 +1,55 @@
 import { TestBed } from '@angular/core/testing'
-import { CanActivateFn } from '@angular/router'
+import {
+  ActivatedRouteSnapshot,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router'
 
+import { PATH } from '@core/constants/path.constant'
+import { PokeTrainerService } from '@core/services/poke-trainer.service'
+import { TRAINER_MOCK } from 'testing/mocks/trainer.mock'
 import { pokemonsGuard } from './pokemons.guard'
 
 describe('pokemonsGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) =>
-    TestBed.runInInjectionContext(() => pokemonsGuard(...guardParameters))
+  const routerSpy = jasmine.createSpyObj('Router', ['parseUrl'])
+
+  const executeGuard = (
+    pokeTrainerServiceMock: Partial<PokeTrainerService>,
+  ) => {
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: PokeTrainerService,
+          useValue: pokeTrainerServiceMock,
+        },
+        {
+          provide: Router,
+          useValue: routerSpy,
+        },
+      ],
+    })
+    return TestBed.runInInjectionContext(() =>
+      pokemonsGuard({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot),
+    )
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({})
   })
 
-  it('should be created', () => {
-    expect(executeGuard).toBeTruthy()
+  it('should return true when trainer is defined', () => {
+    const result = executeGuard({
+      trainer: TRAINER_MOCK,
+    })
+
+    expect(result).toBeTrue()
+  })
+
+  it('should redirect to profile when trainer is not defined', () => {
+    executeGuard({
+      trainer: null,
+    })
+
+    expect(routerSpy.parseUrl).toHaveBeenCalledWith(`/${PATH.PROFILE}`)
   })
 })
