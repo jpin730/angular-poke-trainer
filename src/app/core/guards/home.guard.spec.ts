31bebd5a03e5cc440cae020b7823a1de7a86968e
Jaime Pineda <jpin730@pm.me>
2024-02-13 20:10:23 -0600

    test: :white_check_mark: add tests to home guard

--- src/app/core/guards/home.guard.spec.ts
+++ src/app/core/guards/home.guard.spec.ts
@@ -1,17 +1,66 @@
 import { TestBed } from '@angular/core/testing'
-import { CanActivateFn } from '@angular/router'
+import {
+  ActivatedRouteSnapshot,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router'
 
+import { PATH } from '@core/constants/path.constant'
+import { PokeTrainerService } from '@core/services/poke-trainer.service'
+import { POKEMONS_MOCK } from 'testing/mocks/pokemons.mock'
+import { TRAINER_MOCK } from 'testing/mocks/trainer.mock'
 import { homeGuard } from './home.guard'
 
 describe('homeGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) =>
-    TestBed.runInInjectionContext(() => homeGuard(...guardParameters))
+  const routerSpy = jasmine.createSpyObj('Router', ['parseUrl'])
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({})
+  const executeGuard = (
+    pokeTrainerServiceMock: Partial<PokeTrainerService>,
+  ) => {
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: PokeTrainerService,
+          useValue: pokeTrainerServiceMock,
+        },
+        {
+          provide: Router,
+          useValue: routerSpy,
+        },
+      ],
+    })
+
+    return TestBed.runInInjectionContext(() =>
+      homeGuard({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot),
+    )
+  }
+
+  it('should return true when trainer and pokemons are defined', () => {
+    const pokeTrainerServiceMock: Partial<PokeTrainerService> = {
+      trainer: TRAINER_MOCK,
+      pokemons: POKEMONS_MOCK,
+    }
+
+    const result = executeGuard(pokeTrainerServiceMock)
+
+    expect(result).toBeTrue()
   })
 
-  it('should be created', () => {
-    expect(executeGuard).toBeTruthy()
+  it('should redirect to profile when trainer is not defined', () => {
+    executeGuard({
+      trainer: null,
+      pokemons: POKEMONS_MOCK,
+    })
+
+    expect(routerSpy.parseUrl).toHaveBeenCalledWith(`/${PATH.PROFILE}`)
+  })
+
+  it('should redirect to pokemons when pokemons are not defined', () => {
+    executeGuard({
+      trainer: TRAINER_MOCK,
+      pokemons: null,
+    })
+
+    expect(routerSpy.parseUrl).toHaveBeenCalledWith(`/${PATH.POKEMONS}`)
   })
 })
